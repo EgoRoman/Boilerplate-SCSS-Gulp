faaaba3b561821d345ac9a6f491bddb8dec95611
roman-home <giorgioromanni@gmail.com>
2019-05-30 02:05:20 +0300

    .

--- gulpfile.js
+++ gulpfile.js
@@ -1,72 +1,59 @@
+'use strict';
+
 /**
  * Paths to project folders
  */
-var themePath = './templates/osnova-med/';
-// var projectURL = 'test.r98712ol.beget.tech';
+const themePath = './template/';
+// const projectURL = 'test.r98712ol.beget.tech';
 
-var paths = {
+const paths = {
   styles: {
     input: themePath + 'assets/scss/style.scss',
-    output: themePath + '/css/',
+    output: themePath + 'dist/css/',
     watch: themePath + 'assets/scss/**/*.scss'
-  }
+  },
+  dist: themePath + 'dist'
 };
 
 // Browsers you care about for autoprefixing.
 // Browserlist https        ://github.com/ai/browserslist
-var AUTOPREFIXER_BROWSERS = [
-  'last 2 version',
-  '> 1%',
-  'ie >= 9',
-  'ie_mob >= 10',
-  'ff >= 30',
-  'chrome >= 34',
-  'safari >= 7',
-  'opera >= 23',
-  'ios >= 7',
-  'android >= 4',
-  'bb >= 10'
+const AUTOPREFIXER_BROWSERS = [
+  'last 1 version',
+  'last 2 Chrome versions',
+  'last 2 Firefox versions',
+  'last 2 Opera versions',
+  'last 2 Edge versions'
 ];
 
 /**
  * Gulp Packages
  */
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create(); // Reloads browser and injects CSS. Time-saving synchronised browser testing.
-var sourcemaps = require('gulp-sourcemaps'); // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css)
-var sass = require('gulp-sass'); // Gulp pluign for Sass compilation.
-var autoprefixer = require('gulp-autoprefixer'); // Autoprefixing magic.
-var lineec = require('gulp-line-ending-corrector'); // Consistent Line Endings for non UNIX systems. Gulp Plugin for Line Ending Corrector (A utility that makes sure your files have consistent line endings)
-var rename = require('gulp-rename'); // Renames files E.g. style.css -> style.min.css
-var minifycss = require('gulp-uglifycss'); // Minifies CSS files.
-var filter = require('gulp-filter'); // Enables you to work on a subset of the original files by filtering them using globbing.
+const gulp = require('gulp'),
+  plumber = require('gulp-plumber'),
+  sourcemaps = require('gulp-sourcemaps'), // Maps code in a compressed file (E.g. style.css) back to it’s original position in a source file (E.g. structure.scss, which was later combined with other css files to generate style.css)
+  sass = require('gulp-sass'), // Gulp pluign for Sass compilation.
+  autoprefixer = require('gulp-autoprefixer'), // Autoprefixing magic.
+  lineec = require('gulp-line-ending-corrector'), // Consistent Line Endings for non UNIX systems. Gulp Plugin for Line Ending Corrector (A utility that makes sure your files have consistent line endings)
+  filter = require('gulp-filter'), // Enables you to work on a subset of the original files by filtering them using globbing.
+  browserSync = require('browser-sync').create(), // Reloads browser and injects CSS. Time-saving synchronised browser testing.
+  rename = require('gulp-rename'), // Renames files E.g. style.css -> style.min.css
+  minifycss = require('gulp-uglifycss'), // Minifies CSS files.
 
-/**
- * Gulp Tasks
- */
-gulp.task('browser-sync', function () {
-  browserSync.init({
-    // Project URL.
-    proxy: projectURL,
-    host: projectURL,
-    open: 'external',
-    // Inject CSS changes.
-    // Commnet it to reload browser for every CSS change.
-    injectChanges: true,
-  });
-});
+  del = require('del');
 
 /**
  * Compiles Sass, Autoprefixes it and Minifies CSS.
  */
-gulp.task('styles', function () {
-  return gulp.src(paths.styles.input)
+gulp.task('styles', () => {
+  gulp.src(paths.styles.input)
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(sass({
-      errLogToConsole: true,
-      precision: 10
-    }))
-    .on('error', console.error.bind(console))
+      precision: 10,
+      sourceComments: true
+    })
+      .on('error', sass.logError))
+    // .on('error', console.error.bind(console))
     // .pipe(sourcemaps.write({includeContent: false}))
     // .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
@@ -86,9 +73,42 @@ gulp.task('styles', function () {
 
 /**
  * Watch Tasks.
- *
- * Watches for file changes and runs specific tasks.
  */
-gulp.task('default', ['styles'], function () {
-  gulp.watch(paths.styles.watch, ['styles']); // Reload on SCSS file changes.
+
+gulp.task('watch', ['assemble'], () => {
+  browserSync.init({
+    server: '.',
+    // host: localhost,
+    notify: false,
+    open: true,
+    port: 8080,
+    ui: false
+  });
+
+  gulp.watch(paths.styles.watch, ['styles']);
+  // gulp.watch('*.html').on('change', (e) => {
+  //   if (e.type !== 'deleted') {
+  //     gulp.start('copy-html');
+  //   }
+  // });
+  // gulp.watch('js/**/*.js', ['js-watch']);
 });
+
+gulp.task('assemble', ['clean'], () => {
+  // gulp.start('copy', 'style');
+  gulp.start('styles');
+});
+
+gulp.task('clean', () => {
+  return del(paths.dist);
+});
+
+// gulp.task('copy', ['copy-html', 'scripts', 'style'], () => {
+//   return gulp.src([
+//     'fonts/**/*.{woff,woff2}',
+//     'img/**/*.*'
+//   ], {base: '.'})
+//     .pipe(gulp.dest('build'));
+// });
+
+gulp.task('default', ['watch']);
---
+++ gulpfile_htmlacademy.js
@@ -0,0 +1,137 @@
+'use strict';
+
+const del = require('del'),
+  gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  plumber = require('gulp-plumber'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  server = require('browser-sync').create(),
+  mqpacker = require('css-mqpacker'),
+  minify = require('gulp-csso'),
+  rename = require('gulp-rename'),
+  imagemin = require('gulp-imagemin'),
+  htmlmin = require('gulp-htmlmin'),
+  sourcemaps = require('gulp-sourcemaps'),
+  uglify = require('gulp-uglify'),
+  mocha = require('gulp-mocha'),
+
+  // Babel-huyabel
+  babel = require('rollup-plugin-babel'),
+  commonjs = require('rollup-plugin-commonjs'),
+  rollup = require('gulp-better-rollup'),
+  resolve = require('rollup-plugin-node-resolve');
+
+
+gulp.task('test', () => {
+  gulp.src(['js/**/*.test.js'], { read: false })
+    .pipe(mocha({
+      compilers: ['js:babel-register'],
+      reporter: 'list',
+    }));
+});
+
+gulp.task('style', () => {
+  gulp.src('sass/style.scss')
+    .pipe(plumber())
+    .pipe(sass({ sourceComments: true })
+      .on('error', sass.logError))
+    .pipe(postcss([
+      autoprefixer({
+        browsers: [
+          'last 1 version',
+          'last 2 Chrome versions',
+          'last 2 Firefox versions',
+          'last 2 Opera versions',
+          'last 2 Edge versions'
+        ]
+      }),
+      mqpacker({sort: true})
+    ]))
+    .pipe(gulp.dest('build/css'))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('scripts', () => {
+  return gulp.src('js/main.js')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(rollup({
+      plugins: [
+        resolve({browser: true}), // resolve node_modules
+        commonjs(), // resolve commonjs imports
+        babel({  // use babel to transpile into ES5
+          babelrc: false,
+          exclude: 'node_modules/**',
+          presets: [['env', {modules: false}]],
+          plugins: ['external-helpers']
+        })]
+    }, 'iife'))
+    .pipe(uglify())
+    .pipe(rename('bundle.min.js'))
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('imagemin', ['copy'], () => {
+  return gulp.src('build/img/**/*.{jpg,png,gif}')
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true})
+    ]))
+    .pipe(gulp.dest('build/img'));
+});
+
+
+gulp.task('copy-html', () => {
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'))
+    .pipe(server.stream());
+});
+
+gulp.task('copy', ['copy-html', 'scripts', 'style'], () => {
+  return gulp.src([
+    'fonts/**/*.{woff,woff2}',
+    'img/**/*.*'
+  ], {base: '.'})
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('clean', () => {
+  return del('build');
+});
+
+gulp.task('js-watch', ['scripts'], (done) => {
+  server.reload();
+  done();
+});
+
+gulp.task('serve', ['assemble'], () => {
+  server.init({
+    server: './build',
+    notify: false,
+    open: true,
+    port: 3502,
+    ui: false
+  });
+
+  gulp.watch('sass/**/*.{scss,sass}', ['style']);
+  gulp.watch('*.html').on('change', (e) => {
+    if (e.type !== 'deleted') {
+      gulp.start('copy-html');
+    }
+  });
+  gulp.watch('js/!**/!*.js', ['js-watch']);
+});
+
+gulp.task('assemble', ['clean'], () => {
+  gulp.start('copy', 'style');
+});
+
+gulp.task('build', ['assemble', 'imagemin']);
+
+gulp.task('default', ['serve']);
