0576ae873d45d58957081b81e43182b97554261d
Roman Egorov <giorgioromanni@gmail.com>
2023-04-17 17:25:29 +0300

    dev

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
 // Require PHP 7.3!
+const SITE_TITLE = 'Boilerplate';
 const DIR_BASE = __DIR__ . '/';
 const DIR_TEMPLATES = __DIR__ . '/page-parts/';
 
+const IMG_PATH = '/dist/img/';
+const SVG_SPRITE_SRC = '/dist/img/icons/svg/sprite.svg';
+
 $request = $_SERVER['REQUEST_URI'];
+
+require_once DIR_TEMPLATES . 'header.php';
 
 switch ($request) {
   case '/' :
@@ -18,3 +24,5 @@ switch ($request) {
     require DIR_BASE . '/pages/404.php';
     break;
 }
+
+require_once DIR_TEMPLATES . 'footer.php';
